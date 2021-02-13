f055f7ce318c7cb4537383dd007e63fa553c0ef8
Danko Lučić <ld.is.a.git@gmail.com>
2021-02-13 19:30:00 +0100

    connected view with game logic: - most of the game functionality now works (controling snake, pausing game, scoring) - TODO: changing game speed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, KeyboardEvent, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Field from './Field';
 import Settings from './Settings';
 import Score from './Score';
 import GameOver from './GameOver';
-import { newGame } from './core/newGame';
+import startSnakeGame from './core/snake';
+import { GameState } from './core/types';
 
 const Main = styled.main`
   display: flex;
@@ -31,26 +32,55 @@ const Title = styled.h1`
 `;
 
 const App: FunctionComponent = () => {
-  const gameState = newGame();
+  const [snakeGame, setSnakeGame] = useState(startSnakeGame({}));
+  const [gameState, setGameState] = useState<GameState | undefined>(undefined);
+
+  useEffect(() => {
+    snakeGame.subscribe((newGameState) => setGameState(newGameState));
+    return snakeGame.destroy;
+  }, [snakeGame]);
+
+  useEffect(() => {
+    function handleKeypress(event: KeyboardEvent) {
+      const key = event.key.toLowerCase();
+      if (key === 'w') {
+        snakeGame.changeDirection('up');
+      } else if (key === 's') {
+        snakeGame.changeDirection('down');
+      } else if (key === 'd') {
+        snakeGame.changeDirection('right');
+      } else if (key === 'a') {
+        snakeGame.changeDirection('left');
+      } else if (key === 'p') {
+        snakeGame.togglePause();
+      }
+    }
+    window.addEventListener('keypress', (handleKeypress as unknown) as EventListener);
+
+    return () =>
+      window.removeEventListener('keypress', (handleKeypress as unknown) as EventListener);
+  }, [snakeGame]);
+
+  if (!gameState) {
+    return <></>;
+  }
+
+  function restart() {
+    snakeGame.destroy();
+    setSnakeGame(startSnakeGame({}));
+  }
 
   return (
     <Main>
       <Title>Snake</Title>
       <Game>
         <StyledScore>
-          <Score current={50} />
+          <Score current={gameState.score} />
         </StyledScore>
         <Field gameState={gameState} />
       </Game>
       <Settings delay={6} />
-      {false && (
-        <GameOver
-          onNewGameClick={() => {
-            console.log('');
-          }}
-          finalScore={0}
-        />
-      )}
+      {gameState.gameOver && <GameOver onNewGameClick={restart} finalScore={gameState.score} />}
     </Main>
   );
 };
--- src/Score.tsx
+++ src/Score.tsx
@@ -1,9 +1,23 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
+import startScoring from './core/score';
 
 const Score: FunctionComponent<{ current: number }> = ({ current }) => {
+  const [score, setScore] = useState(0);
+  const [scoreStore] = useState(startScoring());
+
+  useEffect(() => {
+    const unsubscribe = scoreStore.subscribe((newScore) => {
+      setScore(newScore);
+    });
+
+    return unsubscribe;
+  }, [scoreStore]);
+
+  scoreStore.newScore(current);
+
   return (
     <>
-      Score: <span>{current}</span>
+      Score: <span>{score}</span>
     </>
   );
 };
